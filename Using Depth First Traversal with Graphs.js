de4cd8045e45b7833a3fdccaacb584648a2a3d38
leonardevans <lemutugi@gmail.com>
2022-08-17 12:35:05 -0400

    fib memoization

--- Using Depth First Traversal with Graphs.js
+++ Using Depth First Traversal with Graphs.js
@@ -23,6 +23,26 @@ class Node{
         node.edges.push(this)
     }
 
+    connectDirected(node){
+        this.edges.push(node)
+    }
+
+    adjacentNodes(){
+        return this.edges
+    }
+
+    isConnected(node){
+        // return this.edges.map(edge => edge.val).indexOf(node.val) > -1
+        return !!this.edges.find(edge => edge.val === node.val)
+    }
+}
+
+class DirectedNode{
+    constructor(val){
+        this.val = val;
+        this.edges = []
+    }
+
     adjacentNodes(){
         return this.edges
     }
@@ -47,19 +67,45 @@ class Graph{
         keep going deeper into the graph until you hit a leaf node or you are at the end node
         */
 
-        if (start.val === target.val) {
-            console.log("Node Found!")
-            return
+        if (!visited) {
+            visited = new Set()
         }
 
         console.log("Visiting node: " + start.val);
         visited.add(start)
+
+        if (start.val === target.val) {
+            console.log("Node Found!")
+            return
+        }
 
         for(let adjacency of start.edges){
             if (!visited.has(adjacency)) {
                 this.depthFirstTraversal(adjacency, target, visited)
             }
         }
+    }
+
+    topologicalSort(){
+        const visited = new Set()
+        const topOrdering = []
+        for(const node of this.nodes){
+            this.depthFirstTraversalSort(node, visited, topOrdering)
+        }
+        console.log(topOrdering.reverse())
+    }
+
+    depthFirstTraversalSort(start, visited, topOrdering){
+        if (visited.has(start)) return
+
+        visited.add(start)
+        console.log('visiting node: ' , start.val);
+
+        for(const adjacency of start.edges){
+            this.depthFirstTraversalSort(adjacency, visited, topOrdering)
+        }
+
+        topOrdering.push(start.val)
     }
 
 }
@@ -74,8 +120,9 @@ const BOS = new Node('BOS')
 const MIA = new Node('MIA')
 const MCO = new Node('MCO')
 const PBI = new Node('PBI')
+const HKG = new Node('HKG')
 
-const graph = new Graph([DFW, JFK, LAX, HNL, SAN, EWR, BOS, MIA, MCO, PBI])
+const graph = new Graph([DFW, JFK, LAX, HNL, SAN, EWR, BOS, MIA, MCO, PBI, HKG])
 
 DFW.connect(LAX)
 DFW.connect(JFK)
@@ -87,5 +134,59 @@ JFK.connect(MIA)
 MIA.connect(MCO)
 MCO.connect(PBI)
 MIA.connect(PBI)
+SAN.connect(HKG)
 
-graph.depthFirstTraversal(DFW, HNL, new Set)
+graph.depthFirstTraversal(DFW, HKG)
+
+// Big O
+/*
+- Depth First Search has a time complexity of O(v+e) where v is the number of vertices and e is the number of edges
+- The space complexity is O(v) because the visited array will at most store each vertex if we traverse the entire graph
+*/
+
+// What sorts of problems is DFS good for?
+/*
+- Detecting a cycle
+- Discover whether a graph is bipartite
+    - A graph whose vertices can be divinded into two independent sets such that every edge connects a vertex in the first set to one in the second set
+- Finding a path between two nodes (not necessarily the shortest path)
+- Topological sorting
+    A concept where every node in a DAG (directed acyclic graph) appears before all the nodes it points to
+*/
+
+
+// Interview Question
+// Given a DAG, return the topological ordering
+/*
+        A         E
+      /   \    /   \
+    /       C       \
+    B               F
+       \          /
+         \       / 
+            D
+
+Some valid answers would be:
+[A, E, B, D, F, C]
+[E, A, C, B, D, F]
+*/
+
+
+const a = new Node('a')
+const b = new Node('b')
+const c = new Node('c')
+const d = new Node('d')
+const e = new Node('e')
+const f = new Node('f')
+
+const directedGraph = new Graph([a, b, c, d, e, f])
+
+a.connectDirected(c)
+a.connectDirected(b)
+b.connectDirected(d)
+d.connectDirected(f)
+e.connectDirected(c)
+e.connectDirected(f)
+
+console.log('topologicalSort');
+directedGraph.topologicalSort()
---
+++ dynamic-programming/fib memoization.js
@@ -0,0 +1,38 @@
+
+// Write a function `fib(n)` that takes in a number os an argument 
+// The function should retur the n-th number of the Fibonacci sequence
+// The first and the second number of the sequence is 1
+// To generate the next number of the sequence, we sum the previous two
+
+const fib = (n) => {
+    if (n <= 2) return 1
+
+    return fib(n-1) + fib(n-2)
+} // Time: O(2^n), Space: O(n)
+
+
+// console.log(fib(1));
+// console.log(fib(2));
+// console.log(fib(3));
+// console.log(fib(4));
+// console.log(fib(5));
+// console.log(fib(6));
+// console.log(fib(50));
+
+// using memoization
+
+const fibMemo = (n, memo = {}) => {
+    //check if the key n is in the momo object
+    if(n in memo) return memo[n]
+
+    if (n <= 2) return 1
+
+    memo[n] = fibMemo(n-1, memo) + fibMemo(n-2, memo)
+
+    return memo[n]
+} // Time: O(n), Space: O(n)
+
+console.log(fibMemo(6));
+console.log(fibMemo(7));
+console.log(fibMemo(8));
+console.log(fibMemo(50));
